aa0b1d45430e089da45b1a6b2cde1f21196c407e
qzh <2806894439@qq.com>
2024-04-04 16:19:10 +0800

    init

---
+++ src/main/java/com/job/JobpilotApplication.java
@@ -0,0 +1,12 @@
+package com.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class JobpilotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobpilotApplication.class, args);
+    }
+}
---
+++ src/main/java/com/job/util/Result.java
@@ -0,0 +1,35 @@
+package com.job.util;
+
+import com.job.constant.MessageConstants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result implements Serializable {
+    private int code = HttpStatus.OK.value();
+    private String msg = MessageConstants.REQUEST_SUCCESS;
+    private Object data;
+
+    public static Result success() {
+        return new Result();
+    }
+
+    public static Result success(Object data) {
+        Result result = new Result();
+        result.data = data;
+        return result;
+    }
+
+    public static Result fail(int code, String msg) {
+        Result result = new Result();
+        result.code = code;
+        result.msg = msg;
+        return result;
+    }
+}
