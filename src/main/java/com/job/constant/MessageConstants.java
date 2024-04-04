c686864f651d5986f573cd327cfa3710ce5b6870
qzh <2806894439@qq.com>
2024-04-04 16:24:42 +0800

    返回给前端的消息常量

---
+++ src/main/java/com/job/constant/MessageConstants.java
@@ -0,0 +1,12 @@
+package com.job.constant;
+
+/**
+ * 返回信息常量类
+ */
+public class MessageConstants {
+    public static final String MAIL_FORMAT_ERROR = "邮箱格式错误";
+
+    public static final String REQUEST_SUCCESS = "请求成功";
+
+    public static final String MAIL_SEND_FAIL = "邮件发送失败";
+}
