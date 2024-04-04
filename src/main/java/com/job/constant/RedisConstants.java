6a10a6384b09c556dadce60fd7a4a615df4be011
qzh <2806894439@qq.com>
2024-04-04 16:25:03 +0800

    Redis相关常量

---
+++ src/main/java/com/job/constant/RedisConstants.java
@@ -0,0 +1,6 @@
+package com.job.constant;
+
+public class RedisConstants {
+    public static final String REGISTER_CODE_KEY = "jobpilot:register:code:";
+    public static final Long REGISTER_CODE_TTL = 5L;
+}
