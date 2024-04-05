5b6ef33ea63b92058a70098aab0506fb752af444
qzh <2806894439@qq.com>
2024-04-05 16:23:28 +0800

    添加@MapperScan注解

--- src/main/java/com/job/JobpilotApplication.java
+++ src/main/java/com/job/JobpilotApplication.java
@@ -1,9 +1,11 @@
 package com.job;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.job.mapper")
 public class JobpilotApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobpilotApplication.class, args);
