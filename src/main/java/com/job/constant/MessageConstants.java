52c3b074b7e83ac1fccb0ff282552be7fd7fd13c
qzh <2806894439@qq.com>
2024-04-05 16:24:03 +0800

    添加信息

--- src/main/java/com/job/constant/MessageConstants.java
+++ src/main/java/com/job/constant/MessageConstants.java
@@ -5,8 +5,17 @@ package com.job.constant;
  */
 public class MessageConstants {
     public static final String MAIL_FORMAT_ERROR = "邮箱格式错误";
+    public static final String CAPTCHA_FORMAT_ERROR = "验证码格式错误";
+    public static final String USERNAME_FORMAT_ERROR = "用户名格式错误";
+    public static final String PHONE_FORMAT_ERROR = "手机号格式错误";
+    public static final String PASSWORD_FORMAT_ERROR = "密码格式错误";
 
     public static final String REQUEST_SUCCESS = "请求成功";
 
     public static final String MAIL_SEND_FAIL = "邮件发送失败";
+
+    public static final String CAPTCHA_TIMEOUT = "验证码已过期，请重新获取验证码";
+    public static final String CAPTCHA_VERIFY_FAIL = "验证码校验失败";
+
+    public static final String USER_EXISTS = "用户已存在";
 }
